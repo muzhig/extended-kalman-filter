c960f64424a77c29bda560b58fb58ab1c0f3ef09
muzhig <homeip127@gmail.com>
2013-09-06 11:31:45 +0600

    Create Kalman.h

---
+++ Kalman.h
@@ -0,0 +1,16 @@
+#ifndef KALMAN_H_
+#define KALMAN_H_
+#include "KalmanParams.h"
+
+class ExtendedKalman {
+
+public:
+	ExtendedKalman();
+	virtual ~ExtendedKalman();
+	void predict();
+	void correct(const Matrix& z);
+	Matrix X;
+	Matrix P;
+};
+
+#endif /* KALMAN_H_ */
